5c41e6055edf6bd9f20a28938b39a76ce65c34f7
Altaks <altair61.dev@gmail.com>
2023-11-16 15:01:31 +0100

    Added SafeMiner scenario

--- src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/firstpage/SafeMiner.java
+++ src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/firstpage/SafeMiner.java
@@ -3,7 +3,11 @@ package fr.altaks.uhcapi2.views.scenarios.scenarios.firstpage;
 import fr.altaks.uhcapi2.Main;
 import fr.altaks.uhcapi2.views.scenarios.Scenario;
 import fr.mrmicky.fastinv.ItemBuilder;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class SafeMiner implements Scenario {
 
@@ -26,8 +30,19 @@ public class SafeMiner implements Scenario {
         return 19;
     }
 
+    @SuppressWarnings("unused")
+    @EventHandler
+    public void onPlayerTakesDamageFromFire(EntityDamageEvent event){
+        if(event.getEntity() instanceof Player){
+            if(event.getEntity().getLocation().getY() >= 32) return;
+            if(event.getCause() == EntityDamageEvent.DamageCause.LAVA || event.getCause() == EntityDamageEvent.DamageCause.FIRE || event.getCause() == EntityDamageEvent.DamageCause.FIRE_TICK){
+                event.setCancelled(true);
+            }
+        }
+    }
+
     @Override
     public void startScenario(Main main) {
-
+        Bukkit.getPluginManager().registerEvents(this, main);
     }
 }
