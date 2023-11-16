32f23b4dd10f1405454467f940b639c11e60eac5
Altaks <altair61.dev@gmail.com>
2023-11-16 14:57:09 +0100

    Added HasteyBabies, HasteyBoys and StarterTools scenarios

--- src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/firstpage/HasteyBoys.java
+++ src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/firstpage/HasteyBoys.java
@@ -3,7 +3,14 @@ package fr.altaks.uhcapi2.views.scenarios.scenarios.firstpage;
 import fr.altaks.uhcapi2.Main;
 import fr.altaks.uhcapi2.views.scenarios.Scenario;
 import fr.mrmicky.fastinv.ItemBuilder;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.CraftItemEvent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HasteyBoys implements Scenario {
 
@@ -26,8 +33,47 @@ public class HasteyBoys implements Scenario {
         return 14;
     }
 
+    private final ArrayList<Material> tools = new ArrayList<>(Arrays.asList(
+            Material.WOOD_PICKAXE,
+            Material.STONE_PICKAXE,
+            Material.IRON_PICKAXE,
+            Material.GOLD_PICKAXE,
+            Material.DIAMOND_PICKAXE,
+
+            Material.WOOD_AXE,
+            Material.STONE_AXE,
+            Material.IRON_AXE,
+            Material.GOLD_AXE,
+            Material.DIAMOND_AXE,
+
+            Material.WOOD_SPADE,
+            Material.STONE_SPADE,
+            Material.IRON_SPADE,
+            Material.GOLD_SPADE,
+            Material.DIAMOND_SPADE,
+
+            Material.WOOD_HOE,
+            Material.STONE_HOE,
+            Material.IRON_HOE,
+            Material.GOLD_HOE,
+            Material.DIAMOND_HOE,
+
+            Material.SHEARS,
+            Material.FLINT_AND_STEEL
+    ));
+
+    @SuppressWarnings("unused")
+    @EventHandler
+    public void onPlayerCraftsItem(CraftItemEvent event){
+        if(tools.contains(event.getRecipe().getResult().getType())){
+            // crafted item is a tool
+            event.getCurrentItem().addUnsafeEnchantment(Enchantment.DIG_SPEED, 3);
+            event.getCurrentItem().addUnsafeEnchantment(Enchantment.DURABILITY, 3);
+        }
+    }
+
     @Override
     public void startScenario(Main main) {
-
+        Bukkit.getPluginManager().registerEvents(this, main);
     }
 }
