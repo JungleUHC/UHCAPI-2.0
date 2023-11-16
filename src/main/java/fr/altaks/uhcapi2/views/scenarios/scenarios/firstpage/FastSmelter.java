76088ae713d03401fbcb0f4eceefbe18bd2dd237
Altaks <altair61.dev@gmail.com>
2023-11-16 15:07:59 +0100

    Added FastSmelter scenario

--- src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/firstpage/FastSmelter.java
+++ src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/firstpage/FastSmelter.java
@@ -4,6 +4,14 @@ import fr.altaks.uhcapi2.Main;
 import fr.altaks.uhcapi2.core.util.HeadBuilder;
 import fr.altaks.uhcapi2.views.scenarios.Scenario;
 import fr.mrmicky.fastinv.ItemBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Furnace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
 
 public class FastSmelter implements Scenario {
 
@@ -28,8 +36,40 @@ public class FastSmelter implements Scenario {
         return 20;
     }
 
+    private int smeltingMultiplierPercentage = 300;
+    private ArrayList<Furnace> furnaces = new ArrayList<>();
+
     @Override
     public void startScenario(Main main) {
+        Bukkit.getPluginManager().registerEvents(this, main);
 
+        final int period = 20;
+
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                ArrayList<Furnace> toRemove = new ArrayList<>();
+                for(Furnace furnace : furnaces){
+                    if(!furnace.isPlaced() || (furnace.getBlock().getType() != Material.FURNACE && furnace.getBlock().getType() != Material.BURNING_FURNACE)) {
+                        toRemove.add(furnace);
+                        continue;
+                    }
+                    if(furnace.getCookTime() != 0) {
+                        furnace.setCookTime((short) (furnace.getCookTime() + (short) (((double) period) * ((double) smeltingMultiplierPercentage / 100.0d))));
+                    }
+                }
+                furnaces.removeAll(toRemove);
+            }
+
+        }.runTaskTimer(main, 0, 5);
+    }
+
+    @EventHandler
+    public void onPlayerPlacesFurnace(BlockPlaceEvent event) {
+        if(event.getBlock().getType() == Material.FURNACE) {
+            furnaces.add((Furnace) event.getBlock().getState());
+            Main.logDebug("Furnace placed at " + event.getBlock().getLocation().toString() + " added to the list of furnaces.");
+        }
     }
 }
