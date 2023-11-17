e5248e2d7bd4d750cebf66d6c40ab97fc0be1bfb
Altaks <altair61.dev@gmail.com>
2023-11-17 22:32:08 +0100

    Adding TripleOres scneario

--- src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/firstpage/TripleOres.java
+++ src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/firstpage/TripleOres.java
@@ -4,6 +4,18 @@ import fr.altaks.uhcapi2.Main;
 import fr.altaks.uhcapi2.core.util.HeadBuilder;
 import fr.altaks.uhcapi2.views.scenarios.Scenario;
 import fr.mrmicky.fastinv.ItemBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TripleOres implements Scenario {
 
@@ -28,8 +40,49 @@ public class TripleOres implements Scenario {
         return 32;
     }
 
+    private Main main;
+
     @Override
     public void startScenario(Main main) {
+        Bukkit.getPluginManager().registerEvents(this, main);
+        this.main = main;
+    }
+
+    private final ArrayList<Block> blockPlacedByPlayers = new ArrayList<>();
+    private final List<Material> ores = Arrays.asList(
+            Material.COAL_ORE,
+            Material.IRON_ORE,
+            Material.GOLD_ORE,
+            Material.DIAMOND_ORE,
+            Material.EMERALD_ORE,
+            Material.LAPIS_ORE,
+            Material.REDSTONE_ORE,
+            Material.QUARTZ_ORE
+    );
+
+    @SuppressWarnings("unused")
+    @EventHandler
+    public void onPlayerPlacesBlockEvent(BlockPlaceEvent event) {
+        if(event.getPlayer().getGameMode() == GameMode.SURVIVAL) blockPlacedByPlayers.add(event.getBlockPlaced());
+    }
+
+    @SuppressWarnings("unused")
+    @EventHandler
+    public void onPlayerBreaksOreEvent(BlockBreakEvent event) {
+        if(blockPlacedByPlayers.contains(event.getBlock())) {
+            blockPlacedByPlayers.remove(event.getBlock());
+            return;
+        }
+        if(!main.getGameManager().getScenariosController().getEnabledScenarioInstances().containsKey(DirectToInventory.class)){
+            // double the amount of the block if it's an ore
+            for(ItemStack item : event.getBlock().getDrops(event.getPlayer().getItemInHand())){
+                if(ores.contains(event.getBlock().getType())) {
+                    // drop the item triple
+                    event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), item);
+                    event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), item);
+                }
+            }
+        }
 
     }
 }
