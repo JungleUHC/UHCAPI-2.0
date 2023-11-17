06cb536a0af13cb55b6e2a71788ae55045dc225a
Altaks <altair61.dev@gmail.com>
2023-11-17 20:27:57 +0100

    Adding SpeedyMiner

--- src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/firstpage/SpeedyMiner.java
+++ src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/firstpage/SpeedyMiner.java
@@ -3,7 +3,13 @@ package fr.altaks.uhcapi2.views.scenarios.scenarios.firstpage;
 import fr.altaks.uhcapi2.Main;
 import fr.altaks.uhcapi2.views.scenarios.Scenario;
 import fr.mrmicky.fastinv.ItemBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class SpeedyMiner implements Scenario {
 
@@ -28,6 +34,17 @@ public class SpeedyMiner implements Scenario {
 
     @Override
     public void startScenario(Main main) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
 
+                for(Player player : Bukkit.getOnlinePlayers()){
+                    if(player.getGameMode() != GameMode.SPECTATOR && player.getLocation().getBlockY() <= 32){
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 25, 0, false, false));
+                    }
+                }
+
+            }
+        }.runTaskTimer(main, 0, 20);
     }
 }
