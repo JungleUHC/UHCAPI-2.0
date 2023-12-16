0f87267b7033278fbc0f36b2a1d7d27eb4d2a954
Altaks <altair61.dev@gmail.com>
2023-12-16 12:09:13 +0100

    Ajout du scénario Unbreakable

--- src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/secondpage/Unbreakable.java
+++ src/main/java/fr/altaks/uhcapi2/views/scenarios/scenarios/secondpage/Unbreakable.java
@@ -3,7 +3,12 @@ package fr.altaks.uhcapi2.views.scenarios.scenarios.secondpage;
 import fr.altaks.uhcapi2.Main;
 import fr.altaks.uhcapi2.views.scenarios.Scenario;
 import fr.mrmicky.fastinv.ItemBuilder;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Unbreakable implements Scenario {
 
@@ -29,6 +34,16 @@ public class Unbreakable implements Scenario {
 
     @Override
     public void startScenario(Main main) {
+        Bukkit.getPluginManager().registerEvents(this, main);
+    }
 
+    @EventHandler(priority = EventPriority.LOW)
+    public void onItemTakesDamage(PlayerItemDamageEvent event){
+        event.getItem().setDurability((short)0);
+        event.setDamage(0);
+        ItemMeta meta = event.getItem().getItemMeta();
+        meta.spigot().setUnbreakable(true);
+        event.getItem().setItemMeta(meta);
+        event.setCancelled(true);
     }
 }
